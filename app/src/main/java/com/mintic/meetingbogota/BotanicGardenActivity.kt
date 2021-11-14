6949efcd324d94f7db8027ac8372b04e1f355d2c
alvaroperezmoncada <aapm12@hotmail.com>
2021-11-14 13:18:23 -0500

    Vista del Jardín Botánico

---
+++ app/src/main/java/com/mintic/meetingbogota/BotanicGardenActivity.kt
@@ -0,0 +1,11 @@
+package com.mintic.meetingbogota
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BotanicGardenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_botanic_garden)
+    }
+}
