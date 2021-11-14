627d597990853c29b1ef4404214a154efc608302
alvaroperezmoncada <aapm12@hotmail.com>
2021-11-14 12:33:28 -0500

    Botones de sitios, imagen y descripción de la ciudad en vista principal

--- app/src/main/java/com/mintic/meetingbogota/MainActivity.kt
+++ app/src/main/java/com/mintic/meetingbogota/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gold_museum)
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/mintic/meetingbogota/MuseumGoldActivity.kt
+++ app/src/main/java/com/mintic/meetingbogota/MuseumGoldActivity.kt
@@ -3,9 +3,9 @@ package com.mintic.meetingbogota
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class MuseumGoldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_gold_museum)
     }
 }
