e0112e5a2684668895045e6f98ab870ca7bda5d7
alvaroperezmoncada <aapm12@hotmail.com>
2021-11-16 05:25:49 -0500

    Onclick para abrir Jardín, Botánico, Mundo Aventura y Museo del Oro, ajustes diseño interfaces Jardín Botánico y Mundo Aventura

--- app/src/main/java/com/mintic/meetingbogota/MainActivity.kt
+++ app/src/main/java/com/mintic/meetingbogota/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.mintic.meetingbogota
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonGoldMuseum: Button = findViewById(R.id.gold_museum_button)
+        buttonGoldMuseum.setOnClickListener {
+            val intentGoldMuseum = Intent(this, MuseumGoldActivity::class.java)
+            startActivity(intentGoldMuseum)
+        }
+
+        val buttonAdventureWorld: Button = findViewById(R.id.adventure_world_button)
+        buttonAdventureWorld.setOnClickListener {
+            val intentAdventureWorld = Intent(this, MundoAventuraActivity::class.java)
+            startActivity(intentAdventureWorld)
+        }
+
+        val buttonBotanicGarden: Button = findViewById(R.id.botanic_garden_button)
+        buttonBotanicGarden.setOnClickListener {
+            val intentBotanicGarden = Intent(this, BotanicGardenActivity::class.java)
+            startActivity(intentBotanicGarden)
+        }
     }
 }
