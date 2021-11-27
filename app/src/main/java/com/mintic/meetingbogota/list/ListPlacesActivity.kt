e84d36662f9dca92e8c92bddc8364582e8875aee
alvaroperezmoncada <aapm12@hotmail.com>
2021-11-27 07:48:17 -0500

    add funcionalidad App completa con actividades

--- app/src/main/java/com/mintic/meetingbogota/list/ListPlacesActivity.kt
+++ app/src/main/java/com/mintic/meetingbogota/list/ListPlacesActivity.kt
@@ -1,10 +1,16 @@
-package com.mintic.meetingbogota
+package com.mintic.meetingbogota.list
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import com.mintic.meetingbogota.model.Lugar
+import com.mintic.meetingbogota.model.LugarItem
+import com.mintic.meetingbogota.R
+import com.mintic.meetingbogota.detalle.DetalleActivity
+
 
 class ListPlacesActivity : AppCompatActivity() {
 
@@ -19,18 +25,24 @@ class ListPlacesActivity : AppCompatActivity() {
         placesRecyclerView = findViewById(R.id.places_recycler_view)
 
         listPlaces = LoadMockPlacesFromJson()
-        lugaresAdapter = LugaresAdapter(listPlaces)
+        lugaresAdapter = LugaresAdapter(listPlaces, onItemClicked = { onLugarClicked(it) } )
 
         placesRecyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
+            layoutManager =  LinearLayoutManager(context)
             adapter = lugaresAdapter
             setHasFixedSize(false)
         }
 
     }
 
+    private fun onLugarClicked(sitio: LugarItem) {
+        val intent = Intent(this, DetalleActivity::class.java)
+        intent.putExtra("lugar",sitio)
+        startActivity(intent)
+    }
+
     private fun LoadMockPlacesFromJson() : ArrayList<LugarItem> {
-        var lugaresString: String = applicationContext.assets.open("lugaresturisticos.json").bufferedReader().use { it.readText() }
+        val lugaresString: String = applicationContext.assets.open("lugaresturisticos.json").bufferedReader().use { it.readText() }
         val gson = Gson()
         val data = gson.fromJson(lugaresString, Lugar::class.java)
         return data
